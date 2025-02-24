335773ee99c41502f1eaa54459bce19ba112aefe
sandroV1972 <126207083+sandroV1972@users.noreply.github.com>
2025-02-24 14:47:15 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
+import network
+import time
+import credentials
 
+# Wi-Fi credentials
+SSID = ''
+PASSWORD = ''
+
+def connect_to_wifi():
+    station = network.WLAN(network.STA_IF)
+    if not station.isconnected():
+        print("Connecting to WiFi...")
+        station.active(True)
+        station.connect(SSID, PASSWORD)
+        while not station.isconnected():
+            time.sleep(0.5)
+    print("Connected! Network config:", station.ifconfig())
+
+# Run the connection function
+connect_to_wifi()
